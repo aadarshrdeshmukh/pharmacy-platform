ad46bcae01fd22777ee95b46156738e0b3a101f1
aadarshrdeshmukh <aadarshdeshmukh829@gmail.com>
2026-06-20 02:52:02 +0530

    fix: jest config and docker build runs independently

--- app/backend/jest.config.js
+++ app/backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterSetup: ['./tests/setup.js'],
   testMatch: ['**/tests/**/*.test.js'],
   collectCoverageFrom: ['src/**/*.js', '!src/migrations/**', '!src/seeds/**'],
   coverageDirectory: 'coverage',
