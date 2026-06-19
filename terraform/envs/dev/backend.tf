59b4a7dfc0c6ffe16473bee31ffe2147b7032ef0
aadarshrdeshmukh <aadarshdeshmukh829@gmail.com>
2026-06-20 01:01:33 +0530

    feat: fix K8s, Grafana, Vault, Jenkins for local demo

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -12,11 +12,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "pharmacy-platform-dev-terraform-state" # TODO: replace with your value
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"                             # TODO: replace with your value
-    dynamodb_table = "pharmacy-platform-terraform-locks"     # TODO: replace with your value
-    encrypt        = true
-  }
+  # Uncomment for AWS deployment:
+  # backend "s3" {
+  #   bucket         = "pharmacy-platform-dev-terraform-state" # TODO: replace with your value
+  #   key            = "dev/terraform.tfstate"
+  #   region         = "us-east-1"                             # TODO: replace with your value
+  #   dynamodb_table = "pharmacy-platform-terraform-locks"     # TODO: replace with your value
+  #   encrypt        = true
+  # }
 }
